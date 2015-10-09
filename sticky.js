a75336fe271f44f0dcfb0fce7ddb6dd5d8184f4d
Israel Teixeira <israelst@gmail.com>
2015-10-09 00:58:14 -0300

    Add proof of concept.

---
+++ sticky.js
@@ -0,0 +1,60 @@
+function parentTable(el){
+    for(var parent = el; parent = parent.parentNode;){
+        if(parent.tagName === 'TABLE'){
+            return parent;
+        }
+    }
+    return null;
+}
+
+function sumOffsetTops(el){
+    if(el === null) return 0;
+        return el.offsetTop + sumOffsetTops(el.offsetParent);
+}
+
+function preserveWidth(stickable){
+    // It is essential when working with table.
+    var descendants = [].slice.call(stickable.querySelectorAll('*'));
+    descendants.forEach(function(descendant){
+        var style = window.getComputedStyle(descendant);
+        descendant.width = parseInt(style.width, 10);
+    });
+}
+
+function getMarginHolder(stickable){
+    return parentTable(stickable) || stickable.nextElementSibling;
+}
+
+function addStyle(document){
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.sticky{ position: fixed; top: 0; z-index: 99; }';
+    document.getElementsByTagName('head')[0].appendChild(style);
+}
+
+window.addEventListener('load', function(){
+    var stickable = document.getElementById('sticky'),
+        marginHolder = getMarginHolder(stickable),
+        getComputed = window.getComputedStyle.bind(window),
+        marginTop = parseInt(getComputed(marginHolder).marginTop, 10),
+        offset = sumOffsetTops(stickable);
+
+    addStyle(document);
+    preserveWidth(stickable);
+
+    function sticky(){
+        var isVisible = stickable.className === 'sticky';
+
+        if(window.pageYOffset >= offset){
+            if(!isVisible){
+                stickable.className = 'sticky';
+                marginHolder.style.marginTop = (stickable.clientHeight + marginTop) + 'px';
+            }
+        }else if(isVisible){
+            stickable.className = '';
+            marginHolder.style.marginTop = marginTop + 'px';
+        }
+    }
+    sticky();
+    window.onscroll = sticky;
+}, false);
